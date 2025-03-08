65bbc314877cbf829ae15a47ffe1ef6857b173f3
Amr Hussain <amrohossnei@gmail.com>
2025-03-08 22:09:31 +0200

    added the inheritance again in the course app

--- day1_lab/ITIan/course/urls.py
+++ day1_lab/ITIan/course/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/', add_course, name="add"),
     path('update/<int:course_id>', update_course, name="update"),
     path('delete/<int:course_id>', delete_course, name="delete"),
+    path('course/add/', add_course, name='add_new_course'),
     ]
--- day1_lab/ITIan/course/views.py
+++ day1_lab/ITIan/course/views.py
@@ -13,15 +13,18 @@ course_data =  [
 ]
 
 def course_list(request):
-
     return render(request, 'course/show_courses.html', context={'list': course_data})
 
+
 def add_course(request):
-    course_id = request.POST.get('course_id')
-    course_name = request.POST.get('course_name')
-    global course_data
-    course_data.append([course_id, course_name])
-    return HttpResponse("Course stored successfully!")
+    if request.method == 'POST':
+        course_id = request.POST.get('course_id')
+        course_name = request.POST.get('course_name')
+        global course_data
+        course_data.append([course_id, course_name])
+        return HttpResponse("Course stored successfully!")
+    else:
+        return render(request, 'course/add_course.html')
 
 def update_course(request, course_id):
     return HttpResponse(f"<h1>updating course with id {course_id}</h1>")
