0ca8baa0baf70a2d0bb60e8aa13d9894c9118c94
Amr Hussain <amrohossnei@gmail.com>
2025-03-10 22:53:32 +0200

    added trainee to admin page

--- day2_lab/ITIan/trainee/models.py
+++ day2_lab/ITIan/trainee/models.py
@@ -6,12 +6,10 @@ class Trainee(models.Model):
 
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=50)
-    email=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
     phone=models.CharField(max_length=50)
     # addding the personal photo to the root dir of media "media/trainee_photos"
     photo=models.ImageField(upload_to='trainee_photos')
-    # adding a foreign key pointing to the Course table in the course app
-    course_id=models.ForeignKey('course.Course',on_delete=models.CASCADE)
     # adding a flag for activation 
     deleted=models.BooleanField(default=False)
 
