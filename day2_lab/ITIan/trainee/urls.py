717929ee9fbc0423c33dfc38b150c85e523a429c
Amr Hussain <amrohossnei@gmail.com>
2025-03-10 22:14:29 +0200

    mapped to media root in urls.py of Tranee

--- day2_lab/ITIan/trainee/urls.py
+++ day2_lab/ITIan/trainee/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -11,3 +13,5 @@ urlpatterns = [
     # path('trainees/', list_trainees, name="show"),
     # path('show/', show, name="show"),
 ]
+# mapping the images/ to media root
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
