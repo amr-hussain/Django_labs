27060424483469cadc9dfea29c7cd85e9c10183f
Amr Hussain <amrohossnei@gmail.com>
2025-03-10 21:58:10 +0200

    added Trainee Class to tranee/models.py

--- day2_lab/ITIan/trainee/models.py
+++ day2_lab/ITIan/trainee/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Trainee(models.Model):
+
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    phone=models.CharField(max_length=50)
+    # adding a foreign key pointing to the Course table in the course app
+    course_id=models.ForeignKey('course.Course',on_delete=models.CASCADE)
 
