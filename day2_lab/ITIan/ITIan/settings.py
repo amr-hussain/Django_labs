16587ae7572101b6287f77e4ab7b3a4c0a00da8a
Amr Hussain <amrohossnei@gmail.com>
2025-03-11 23:43:54 +0200

    linked courses to models.py

--- day2_lab/ITIan/ITIan/settings.py
+++ day2_lab/ITIan/ITIan/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'course.apps.CourseConfig',#'track',#not prof
+    'course.apps.CourseConfig',
     'trainee.apps.TraineeConfig',
 ]
 
