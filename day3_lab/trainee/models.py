1cf1c282a482e639e1eef2f67145e6dcf124cada
Amr Hussain <amrohossnei@gmail.com>
2025-03-13 14:36:18 +0200

    added method overriding to save to delete old images when updating

--- day3_lab/trainee/models.py
+++ day3_lab/trainee/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.files.storage import default_storage #to get and modify the file path
 # Create your models here.
 
 class Trainee(models.Model):
@@ -15,3 +15,23 @@ class Trainee(models.Model):
 
     def __str__(self):
         return self.name 
+    
+    # performing method overriding on save() to add delete old image functionality
+    def save(self, *args, **kwargs):
+        
+        # old_record = [id(pk), old_name, old_col  , , old_photo]
+        # new_record = [id(pk), new_name, new_col , , new_photo]
+        # we still don't know if the new photo is the same as the old photo..
+
+        # first making sure the obj (record) has a primary key
+        # which inform that this is not a new record
+        if self.pk:
+            old_record = Trainee.objects.get(pk=self.pk)
+            # we don't have to check for old_record.photo if the column is required
+            if old_record.photo and self.photo != old_record.photo:
+                # to prevent raising error if the old image path was lost somehow.
+                if default_storage.exists(old_record.photo.path):
+                    default_storage.delete(old_record.photo.path)
+
+        # now we append the default behavior of the save method
+        super().save(*args, **kwargs)
